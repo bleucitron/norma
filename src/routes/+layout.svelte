b4d8987f7b4d985397037dafac289ae6afa8f028
Gaëtan <gaetan.moreau@agence-gw.com>
2023-12-15 14:51:07 +0100

    get if user authentication state

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,29 @@
-<script>
-  	import '../style/global.scss';
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { invalidate } from '$app/navigation';
+	import Header from '$lib/components/Header.svelte';
 
+	export let data;
+
+	$: ({ supabase } = data);
+	$: user = data.user;
+
+	onMount(async () => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== _session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
 </script>
 
-  
-  
-  <main>
-      <slot />
-  </main>
+<Header {user} />
+<main>
+	<slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,7 @@
 	import Button from '@smui/button';
 	import Card, { Content, Actions, ActionButtons } from '@smui/card';
 	export let data;
-
-	async function handleSubmit(event) {
-        event.preventDefault();
-        try {
-			const response = await fetch(event.target.action, {
-				method: event.target.method,
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: new URLSearchParams(new FormData(event.target))
-            });
-			console.log("success logout");
-            if (!response.ok) {
-                throw new Error(`Error: ${response.statusText}`);
-            }
-        } catch (error) {}
-    }
 </script>
-
-<header>
-	<a href="/admin/login">Administration</a>
-	<form action="/" method="post" on:submit={handleSubmit}>
-		<button type="submit">Logout</button>
-	</form>
-</header>
 
 <h1>Norma</h1>
 
