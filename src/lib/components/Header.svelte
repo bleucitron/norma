bb46bcb522f4edfcc96af90d14d85df4c5042793
corentincorre <57666536+corentincorre@users.noreply.github.com>
2024-01-12 11:07:34 +0100

    fix urls (#41)

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -11,20 +11,20 @@
 		setTimeout(() => goto('/'), 0);
 		toast.push('Déconnexion réussie', {
 			theme: {
-                    '--toastBackground': '#4caf50',
-                    '--toastProgressBackground': '#81c784',
-                    '--toastProgressAfterBackground': '#a5d6a7',
-                    '--toastColor': '#fff',
-                    '--toastProgressColor': '#fff',
-                    '--toastProgressAfterColor': '#fff',
-                },						
+				'--toastBackground': '#4caf50',
+				'--toastProgressBackground': '#81c784',
+				'--toastProgressAfterBackground': '#a5d6a7',
+				'--toastColor': '#fff',
+				'--toastProgressColor': '#fff',
+				'--toastProgressAfterColor': '#fff'
+			}
 		});
 	};
 </script>
 
 <header>
 	<div class="container">
-		<a href="/" class="header__logo"><img src="/assets/norma-logo.png" /></a>
+		<a href="/" class="header__logo"><img src="/assets/norma-logo.png" alt="logo" /></a>
 		{#if user}
 			<div class="btn__container">
 				<a class="btn" href="/admin">Accueil</a>
