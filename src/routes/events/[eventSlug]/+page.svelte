729048fbb70dd7b617633cea623b483fd05a7f22
antoineaubeneau <57666445+antoineaubeneau@users.noreply.github.com>
2024-01-12 16:41:14 +0100

    Optimisation last elements and routes

--- src/routes/events/[eventSlug]/+page.svelte
+++ src/routes/events/[eventSlug]/+page.svelte
@@ -5,27 +5,37 @@
 
 <section class="event__container">
 	<div class="wrapper">
-		<div class="card__img">
-	<div>
-		<div>
-			{#if data.logo}
-				<img src={data.logo.publicUrl} alt={`Logo de ${data.title}`} />
-			{/if}
-		</div>
-		<div>
-			<h1>{data.title}</h1>
-			<p>Type de l'événement : {data.activityType}</p>
-			<p>{data.description}</p>
-			<h2>Prix :</h2>
-			<ul>
-				{#each data.tiers as price}
-					<li>{price.label} : {(price.price / 100).toFixed(2).replace('.', ',')} €</li>
-				{/each}
-			</ul>
-		</div>
-		<div class="btn__container">
-			<a class="btn" href="{data.formSlug}/register">S'inscrire</a>
-		</div>
+		<section class="card__img">
+			<div>
+				<div>
+					{#if data.logo}
+						<img src={data.logo.publicUrl} alt={`Logo de ${data.title}`} />
+					{/if}
+				</div>
+				<div>
+					<h1>{data.title}</h1>
+					<p>Type de l'événement : {data.activityType}</p>
+					<p>{data.description}</p>
+					<h2>Prix :</h2>
+					<ul>
+						{#each data.tiers as price}
+							<li>{price.label} : {(price.price / 100).toFixed(2).replace('.', ',')} €</li>
+						{/each}
+					</ul>
+					<iframe
+						id="haWidget"
+						allowtransparency="true"
+						scrolling="auto"
+						src={data.url + '/widget'}
+						style="width: 100%; height: 750px; border: none;"
+						title={data.title}
+					></iframe>
+				</div>
+				<div class="btn__container">
+					<a class="btn" href="{data.formSlug}/register">S'inscrire</a>
+				</div>
+			</div>
+		</section>
 	</div>
 </section>
 
@@ -35,7 +45,8 @@
 			width: 100%;
 		}
 		iframe,
-		h1, h2 {
+		h1,
+		h2 {
 			margin-top: 1rem;
 			margin-bottom: 1rem;
 		}
