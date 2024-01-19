57eaab1ed6b4875f147e64df243ec8abe03780fd
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2024-01-19 17:57:52 +0100

    [fix] fix form for user register

--- src/routes/events/[slug]/register/+page.server.ts
+++ src/routes/events/[slug]/register/+page.server.ts
@@ -1,31 +1,35 @@
-import { fail, redirect } from '@sveltejs/kit';
+// import { fail, redirect } from '@sveltejs/kit';
 
-export const actions = {
-	default: async ({ params, cookies, request, locals: { supabase } }) => {
-		const formData = await request.formData();
-		const level = formData.get('level')?.toString();
-		const role = formData.get('role')?.toString();
-		if (!(level && role)) {
-			console.log('champs non valide');
-			return fail(400, {
-				error: 'champs non valide'
-			});
-		}
-		let dancer = JSON.parse(atob(cookies.get('dancer')));
-		const { error: insetError } = await supabase.from('event_dancers').insert({
-			dancer_id: dancer.id,
-			role: role,
-			level: level,
-			event: params.slug
-		});
-		if (insetError) {
-			console.log(insetError);
-			return fail(400, {
-				error: 'Une erreur est survenue'
-			});
-		}
+// export const actions = {
+// 	default: async ({ params, cookies, request, locals: { supabase } }) => {
+// 		const formData = await request.formData();
+// 		const level = formData.get('level')?.toString();
+// 		const role = formData.get('role')?.toString();
+// 		if (!(level && role)) {
+// 			console.log('champs non valide');
+// 			return fail(400, {
+// 				error: 'champs non valide'
+// 			});
+// 		}
+// 		let dancer = JSON.parse(atob(cookies.get('dancer')));
+// 		const { error: insetError } = await supabase.from('event_dancers').insert({
+// 			dancer_id: dancer.id,
+// 			email: dancer.email,
+// 			firstname: dancer.firstname,
+// 			lastname: dancer.lastname,
+// 			partnaire: dancer.partnaire,
+// 			role: role,
+// 			level: level,
+// 			event: params.slug
+// 		});
+// 		if (insetError) {
+// 			console.log(insetError);
+// 			return fail(400, {
+// 				error: 'Une erreur est survenue'
+// 			});
+// 		}
 
-        //TODO: Implémenter la modal de succés
-        throw redirect(302, '/events/' + params.slug);
-    },
-};
+//         //TODO: Implémenter la modal de succés
+//         throw redirect(302, '/events/' + params.slug);
+//     },
+// };
