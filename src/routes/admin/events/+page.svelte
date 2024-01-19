699eb56b344268f77c455de2afe025ad61657d26
Gaëtan <gaetan.moreau@agence-gw.com>
2024-01-19 11:05:00 +0100

    improve admin template

--- src/routes/admin/events/+page.svelte
+++ src/routes/admin/events/+page.svelte
@@ -4,7 +4,12 @@
 
 <svelte:head></svelte:head>
 
-<h1>Tous vos événements</h1>
+<div class="header-container">
+	<h1>Tous vos événements</h1>
+	<div class="btn__container">
+		<a href={`/admin`} class="btn">Retour au dashboard</a>
+	</div>
+</div>
 
 <ul class="events-list">
 	{#each data.data as event}
@@ -34,47 +39,10 @@
 </ul>
 
 <style lang="scss">
-	:global(.mdc-card__action-buttons) {
-		.event-img {
-			width: 100%;
-			height: 240px;
-			object-fit: cover;
-		}
-		.event__item {
-			box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-		}
-		.events-list {
-			padding-left: 0;
-			display: grid;
-			grid-template-columns: repeat(2, 1fr);
-			gap: 4rem;
-			margin-bottom: 2rem;
-		}
-		.events-list .event {
-			list-style: none;
-		}
-		.card__content {
-			padding: 2rem;
-			gap: 1rem;
-			display: flex;
-			flex-direction: column;
-
-			p {
-				overflow: hidden;
-				text-overflow: ellipsis;
-				display: -webkit-box;
-				-webkit-line-clamp: 3;
-				-webkit-box-orient: vertical;
-			}
-		}
-		@media (max-width: 768px) {
-			h1 {
-				font-size: 3rem;
-				line-height: calc(3rem * 1.4);
-			}
-			.events-list {
-				grid-template-columns: repeat(1, 1fr);
-			}
-		}
+	.header-container {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-bottom: 34px;
 	}
 </style>
--- src/routes/admin/users/+page.svelte
+++ src/routes/admin/users/+page.svelte
@@ -1,23 +1,23 @@
 <script>
 	import { users } from '../../../data/users.json';
-  
+
 	let searchTerm = '';
 	let sortColumn = '';
 	let sortOrder = 1;
 	let sortedUsers = users.slice();
-  
-	$: filteredUsers = sortedUsers.filter(user => {
-	  const trimmedSearchTerm = searchTerm.trim();
-	  if (trimmedSearchTerm === '') {
-		return true;
-	  }
-  
-	  const fullName = `${user.firstname} ${user.lastname}`.toLowerCase();
-	  return fullName.includes(trimmedSearchTerm.toLowerCase());
+
+	$: filteredUsers = sortedUsers.filter((user) => {
+		const trimmedSearchTerm = searchTerm.trim();
+		if (trimmedSearchTerm === '') {
+			return true;
+		}
+
+		const fullName = `${user.firstname} ${user.lastname}`.toLowerCase();
+		return fullName.includes(trimmedSearchTerm.toLowerCase());
 	});
-  
+
 	$: numberOfFilteredUsers = filteredUsers.length;
-  
+
 	function toggleSort(column) {
 		if (column === sortColumn) {
 			sortOrder *= -1;
@@ -35,7 +35,10 @@
 		} else {
 			sortedUsers = filteredUsers.slice().sort((a, b) => {
 				if (column === 'name') {
-					return sortOrder * (a.firstname + ' ' + a.lastname).localeCompare(b.firstname + ' ' + b.lastname);
+					return (
+						sortOrder *
+						(a.firstname + ' ' + a.lastname).localeCompare(b.firstname + ' ' + b.lastname)
+					);
 				} else {
 					return sortOrder * a[column].localeCompare(b[column]);
 				}
@@ -54,53 +57,65 @@
 		const [day, month, year] = dateString.split('/');
 		return new Date(`${year}-${month}-${day}`);
 	}
-  </script>
-  
-  <section class="users__container">
-	<h1>Listes des utilisateurs</h1>
+</script>
+
+<section class="users__container">
+	<div class="header-container">
+		<h1>Listes des utilisateurs</h1>
+		<div class="btn__container">
+			<a href={`/admin`} class="btn">Retour au dashboard</a>
+		</div>
+	</div>
 	{#if numberOfFilteredUsers === 1}
-	  <p>Il y a {numberOfFilteredUsers} utilisateur trouvé</p>
+		<p>Il y a {numberOfFilteredUsers} utilisateur trouvé</p>
 	{:else if numberOfFilteredUsers > 1}
-	  <p>Il y a {numberOfFilteredUsers} utilisateurs trouvés</p>
+		<p>Il y a {numberOfFilteredUsers} utilisateurs trouvés</p>
 	{:else}
-	  <p>Aucun utilisateur trouvé</p>
+		<p>Aucun utilisateur trouvé</p>
 	{/if}
 	<div class="filters__container">
 		<div class="search__container">
 			<span class="search-icon">🔍</span>
 			<input type="text" bind:value={searchTerm} placeholder="Rechercher un utilisateur" />
-
 		</div>
-	  <button on:click={resetState}>Réinitialiser</button>
+		<button on:click={resetState}>Réinitialiser</button>
 	</div>
 	<table>
-	  <thead>
-		<tr>
-		  <th on:click={() => toggleSort('name')}>
-			Nom
-			<span class:desc={sortOrder === -1 && sortColumn === 'name'} class="sort-icon"></span>
-		  </th>
-		  <th on:click={() => toggleSort('created_at')}>
-			Inscrit le
-			<span class:desc={sortOrder === -1 && sortColumn === 'created_at'} class="sort-icon"></span>
-		  </th>
-		</tr>
-	  </thead>
-	  <tbody>
-		{#if numberOfFilteredUsers === 0}
-		  <tr>
-			<td>Aucun utilisateur trouvé</td>
-		  </tr>
-		{:else}
-		  {#each filteredUsers as user}
+		<thead>
 			<tr>
-			  <td>{user.firstname} {user.lastname}</td>
-			  <td>{user.created_at}</td>
+				<th on:click={() => toggleSort('name')}>
+					Nom
+					<span class:desc={sortOrder === -1 && sortColumn === 'name'} class="sort-icon"></span>
+				</th>
+				<th on:click={() => toggleSort('created_at')}>
+					Inscrit le
+					<span class:desc={sortOrder === -1 && sortColumn === 'created_at'} class="sort-icon"
+					></span>
+				</th>
 			</tr>
-		  {/each}
-		{/if}
-	  </tbody>
+		</thead>
+		<tbody>
+			{#if numberOfFilteredUsers === 0}
+				<tr>
+					<td>Aucun utilisateur trouvé</td>
+				</tr>
+			{:else}
+				{#each filteredUsers as user}
+					<tr>
+						<td>{user.firstname} {user.lastname}</td>
+						<td>{user.created_at}</td>
+					</tr>
+				{/each}
+			{/if}
+		</tbody>
 	</table>
-  </section>
-  
-  
+</section>
+
+<style lang="scss">
+	.header-container {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-bottom: 34px;
+	}
+</style>
