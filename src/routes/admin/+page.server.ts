31f007b522edf0ea86b6a7f6380751de38347d6b
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2024-01-26 17:34:09 +0100

    [add] registered user in admin

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,14 +1,32 @@
-import { access_token } from '$lib/server/accessToken'
+import { access_token } from '$lib/server/accessToken';
 import { get } from 'svelte/store';
 
-export async function load({ fetch }) {
-    const events = await fetch("https://api.helloasso.com/v5/organizations/norma-ecv/forms?pageIndex=1&pageSize=20&formTypes=event", {
-        method: "GET",
-        headers: {
-            authorization: 'Bearer ' + get(access_token)
-        }
-    }).then(resp => resp.json())
-    return {
-        events: events.data
-    }
+export async function load({ fetch, locals }) {
+	try {
+		const eventsResponse = await fetch(
+			'https://api.helloasso.com/v5/organizations/norma-ecv/forms?pageIndex=1&pageSize=20&formTypes=event',
+			{
+				method: 'GET',
+				headers: {
+					authorization: 'Bearer ' + get(access_token)
+				}
+			}
+		);
+		const eventsData = await eventsResponse.json();
+		const events = eventsData.data;
+
+		const { data: dancers, error } = await locals.supabase.from('dancers').select('*');
+		if (error) {
+			console.error('Error fetching users from Supabase', error);
+			return { events: [], dancers: [] };
+		}
+
+		return {
+			events: events,
+			dancers: dancers
+		};
+	} catch (error) {
+		console.error('Error in load function', error);
+		return { events: [], dancers: [] };
+	}
 }
