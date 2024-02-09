f2b2eac79c7af6633d4a2d53dab71fa02e549796
antoineaubeneau <57666445+antoineaubeneau@users.noreply.github.com>
2024-02-09 11:17:01 +0100

    addpageuserbyhand

--- src/routes/admin/events/adduser/[slug]/+page.server.ts
+++ src/routes/admin/events/adduser/[slug]/+page.server.ts
@@ -1,8 +1,8 @@
-// import { error, fail, redirect } from '@sveltejs/kit';
+// import { fail, redirect } from '@sveltejs/kit';
 // import { access_token } from '$lib/server/accessToken';
 // import { get } from 'svelte/store';
 
-// export async function load({ cookies, params, fetch }) {
+// export async function load({ params, fetch }) {
 
 // }
 // export const actions = {
@@ -32,110 +32,13 @@
 //                     authorization: 'Bearer ' + get(access_token)
 //                 }
 //             }
-//         ).then((resp) => resp.json());
-//         let { data: registrationCount, error: countError } = await supabase
-//             .from('dancers')
-//             .select('*(count)')
-//             .eq('event', params.slug)
-//             .eq('state', 'inscrit')
-//         if (countError) {
-//             return fail(400, {
-//                 error: "Erreur lors de la recherche du nombre d'inscrits"
-//             });
-//         }
-
-//         if (!Number(registrationCount)) {
-//             registrationCount = 0
-//         }
-//         if (registrationCount >= 50) {
-
-//             return registerWaiting(params, formData, supabase)
-//         }
-//         const check_role = await checkRole(params.slug, role, level, supabase)
-
-//         if (check_role) {
-//             if (partnaire_email) {
-//                 const { data: partenaire, error: partenaireError } = await supabase
-//                     .from('dancers')
-//                     .select('*(count)')
-//                     .eq('event', params.slug)
-//                     .eq('email', partnaire_email)
-//                 if (partenaireError) {
-//                     return fail(400, {
-//                         error: "Erreur lors de la recherche du partenaire renseigné"
-//                     });
-//                 }
-//                 if (partenaire) {
-//                     if (partenaire.state === 'Attente') {
-//                         //envoi auto mail pour payer
-//                         setDancerOrderWaiting(params, partnaire_email, supabase)
-//                     }
-//                 } else {
-//                     sendInvitationMail(partnaire_email)
-//                 }
-
-//             }
-//             return registerOk(params, formData, supabase);
-
-//         } else {
-//             if (partnaire_email) {
-//                 const { data: partenaire, error: partenaireError } = await supabase
-//                     .from('dancers')
-//                     .select('*(count)')
-//                     .eq('event', params.slug)
-//                     .eq('email', partnaire_email)
-
-//                 if (partenaire) {
-//                     if (partenaire.state === 'Inscrit') {
-//                         return registerOk(params, formData, supabase);
-//                     }
-//                 } else {
-//                     sendInvitationMail(partnaire_email)
-//                 }
-//             }
-//             return registerWaiting(params, formData, supabase)
+//         ).then((resp) => resp.json())
+//         const save = await register(params, formData, supabase, 2)
+//         return {
+//             success: save
 //         }
 //     },
 // };
-
-// async function checkRole(event, role, level, supabase) {
-//     const { data: selectedCount, error: selectedCountError } = await supabase
-//         .from('dancers')
-//         .select('*(count)')
-//         .eq('event', event)
-//         .eq('role', role)
-//         .eq('level', level)
-
-//     const { data: oppositeCount, error: oppositeCountError } = await supabase
-//         .from('dancers')
-//         .select('*(count)')
-//         .eq('role', (role === 'leader' ? 'suiveur' : 'leader'))
-//         .eq('level', level)
-
-//     return selectedCount <= oppositeCount + 2;
-// }
-
-// async function registerWaiting(params, formData, supabase) {
-//     try {
-//         await register(params, formData, supabase, 'Attente')
-//     } catch (e) {
-//         return fail(400, {
-//             error: e
-//         })
-//     }
-//     throw redirect(302, '/events/' + params.slug + '/reservation');
-
-// }
-// async function registerOk(params, formData, supabase) {
-//     try {
-//         await register(params, formData, supabase, 'Reglement en cours')
-//     } catch (e) {
-//         return fail(400, {
-//             error: e
-//         })
-//     }
-//     throw redirect(302, '/events/' + params.slug + '/commande');
-// }
 
 // async function register(params, formData, supabase, state) {
 //     const email = formData.get('email')?.toString();
@@ -150,21 +53,6 @@
 //         .select()
 //         .eq('email', email)
 //         .eq('event', params.slug)
-
-//     if (alreadyExist) {
-//         switch (alreadyExist.state) {
-//             case 'Reglement en cours':
-//                 throw redirect(302, '/events/' + params.slug + '/commande');
-//             case 'Inscrit':
-//                 throw redirect(302, '/events/' + params.slug + '/confirmation');
-//             case 'Attente':
-//                 throw redirect(302, '/events/' + params.slug + '/reservation');
-//             default:
-//                 return fail(400, {
-//                     error: 'Erreur'
-//                 })
-//         }
-//     }
 
 //     const { error: insertError } = await supabase
 //         .from('dancers')
@@ -184,18 +72,4 @@
 //         });
 //     }
 //     return true;
-// }
-// function sendInvitationMail(email) {
-
-// }
-
-// async function setDancerOrderWaiting(params, partnaire_email, supabase) {
-//     const { error } = await supabase
-//         .from('countries')
-//         .update({ state: 'Reglement en cours' })
-//         .eq('email', partnaire_email)
-//         .eq('event', params.slug);
-//     if (!error) {
-//         //send mail auto pour payer
-//     }
 // }
