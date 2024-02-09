dc794903fb27c6c75d309e569d4f9270464e13e6
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2024-02-09 15:51:40 +0100

    [fix] Type pass by event + sales ticket by event

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -3,30 +3,42 @@ import { get } from 'svelte/store';
 
 export async function load({ fetch, locals }) {
 	try {
-		const eventsResponse = await fetch(
-			'https://api.helloasso.com/v5/organizations/norma-ecv/forms?pageIndex=1&pageSize=20&formTypes=event',
-			{
+		const [eventsResponse, ordersResponse] = await Promise.all([
+			fetch(
+				'https://api.helloasso.com/v5/organizations/norma-ecv/forms?pageIndex=1&pageSize=20&formTypes=event',
+				{
+					method: 'GET',
+					headers: {
+						authorization: 'Bearer ' + get(access_token)
+					}
+				}
+			),
+			fetch('https://api.helloasso.com/v5/organizations/norma-ecv/items', {
 				method: 'GET',
 				headers: {
 					authorization: 'Bearer ' + get(access_token)
 				}
-			}
-		);
+			})
+		]);
 		const eventsData = await eventsResponse.json();
 		const events = eventsData.data;
+
+		const ordersData = await ordersResponse.json();
+		const orders = ordersData.data;
 
 		const { data: dancers, error } = await locals.supabase.from('dancers').select('*');
 		if (error) {
 			console.error('Error fetching users from Supabase', error);
-			return { events: [], dancers: [] };
+			return { events: [], dancers: [], orders: [] };
 		}
 
 		return {
 			events: events,
-			dancers: dancers
+			dancers: dancers,
+			orders: orders
 		};
 	} catch (error) {
 		console.error('Error in load function', error);
-		return { events: [], dancers: [] };
+		return { events: [], dancers: [], orders: [] };
 	}
 }
