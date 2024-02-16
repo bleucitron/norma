477022e56faf12636dee79d044d7bcfcb0baac31
Gaëtan <gaetan.moreau@agence-gw.com>
2024-02-16 16:56:28 +0100

    add attribution partner id for user added by admin

--- src/routes/admin/events/adduser/[slug]/+page.server.ts
+++ src/routes/admin/events/adduser/[slug]/+page.server.ts
@@ -136,11 +136,71 @@ async function register(
 		state: state,
 		role: role,
 		level: level,
-		event: params.slug,
-		partner_email: partnaire_email
+		event: params.slug
 	});
 	if (insertError) {
 		error = 'Une erreur est survenue :' + insertError;
 	}
+
+	if (partnaire_email) {
+		const oppositeRole = role === Role.Leader ? Role.Suiveur : Role.Leader;
+		const { error: insertPartnerError } = await supabase.from('dancers').insert({
+			email: partnaire_email,
+			state: state,
+			role: oppositeRole,
+			level: level,
+			event: params.slug
+		});
+		dancerIdAttribution(email, partnaire_email, supabase, params);
+		if (insertPartnerError) {
+			throw new Error("Erreur lors de l'enregistrement");
+		}
+	}
 	return error;
+}
+
+// Récupère les id du danceur et de son partenaire afin de les relier entre eux dans la BD
+async function dancerIdAttribution(
+	email: string,
+	partner_email: string,
+	supabase: NormaDatabase,
+	params: { slug: string }
+) {
+	// Sélection du danseur principal
+	const { data: dancer } = await supabase
+		.from('dancers')
+		.select()
+		.eq('email', email)
+		.eq('event', params.slug)
+		.maybeSingle();
+
+	// Mise à jour de l'ID du partenaire pour le danseur principal
+	if (dancer?.id) {
+		await updatePartnerId(supabase, partner_email, params.slug, dancer.id);
+	}
+
+	// Sélection du partenaire
+	const { data: dancerPartner } = await supabase
+		.from('dancers')
+		.select()
+		.eq('email', partner_email)
+		.eq('event', params.slug)
+		.maybeSingle();
+
+	// Mise à jour de l'ID du partenaire pour le partenaire
+	if (dancerPartner?.id) {
+		await updatePartnerId(supabase, email, params.slug, dancerPartner.id);
+	}
+}
+async function updatePartnerId(
+	supabase: NormaDatabase,
+	email: string,
+	enventSlug: string,
+	partnerId: number
+) {
+	await supabase
+		.from('dancers')
+		.update({ partner_id: partnerId })
+		.eq('email', email)
+		.eq('event', enventSlug);
 }
