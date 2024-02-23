c4d6d5df416f6cfcf891b8e5dc8963228ee95dd2
Corentin Corre <corentin.corre@hotmail.fr>
2024-02-23 10:38:17 +0100

    add new test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -23,3 +23,14 @@ test('has title', async ({ page }) => {
 	// Expect a title "to contain" a substring.
 	await expect(page).toHaveTitle(/Norma/);
 });
+
+test('has events list', async ({ page }) => {
+	await page.goto('http://localhost:5173/');
+
+	// Expect a title "to contain" a substring.
+	const list = page.locator('.events-list:not(.events-archived)');
+	await expect(list).toBeVisible();
+
+	const event = page.locator('.events-list:not(.events-archived) .event');
+	await expect(await event.count()).toBeGreaterThanOrEqual(1);
+});
