0784a684b5b55c791c4ddcc56c73c92f771421ed
corentincorre <57666536+corentincorre@users.noreply.github.com>
2024-03-15 13:31:16 +0100

    Helloassolistener (#117)
    * create route to listen hello asso events
    
    * log order on payment page
    
    * save order to db
    
    * listen hello asso events

---
+++ src/routes/helloasso/events/+server.ts
@@ -0,0 +1,67 @@
+import { json } from '@sveltejs/kit'
+import { State } from '$lib/types/norma';
+import { supabase } from '$lib/supabase';
+
+type Order = {
+    order: {
+        id: number;
+        formSlug: string;
+
+    };
+    payer: {
+        email: string;
+        firstName: string;
+        lastName: string;
+
+    };
+}
+
+type HelloAssoNotificationBody<T = unknown> = {
+    eventType: string;
+    data: T;
+}
+
+
+export async function GET(event) {
+    return new Response('Hello')
+}
+
+
+export async function POST(event) {
+    const body: HelloAssoNotificationBody = await event.request.json()
+    if (body.eventType) {
+        switch (body.eventType) {
+            case 'Order':
+                const data = body.data as Order
+                const order_id = data.order.id
+                const email = data.payer.email
+                const event = data.order.formSlug
+
+
+                const { error } = await supabase
+                    .from('dancers')
+                    .update({ order_id: order_id, state: State.Inscrit })
+                    .eq('event', event)
+                    .eq('email', email)
+                if (error) {
+                    console.log(error)
+                }
+                break;
+
+
+
+            case 'Payment':
+                console.log('Payment')
+                break;
+            case 'Form':
+                console.log('Form')
+                break;
+            case 'Organization':
+                console.log('Organization')
+                break;
+            default:
+                break;
+        }
+    }
+    return json({ success: true })
+}
