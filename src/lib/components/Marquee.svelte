a612f667553bd5c84e01bd1fcce7cb9f37e04ce7
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2024-04-12 16:19:40 +0200

    [add] marquee

---
+++ src/lib/components/Marquee.svelte
@@ -0,0 +1,7 @@
+<div class="marquee-container">
+	<div class="marquee">
+		<div class="marquee__inner first">
+			<span>Norma</span><span>Norma</span><span>Norma</span><span>Norma</span>
+		</div>
+	</div>
+</div>
