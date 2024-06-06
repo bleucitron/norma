4148c17ca66d3f126aa443f07642a7bcd28dd99d
Corentin Corre <corentin.corre@hotmail.fr>
2024-06-06 17:23:25 +0200

    fix ticket stat and x2 tickets

--- src/routes/events/[slug]/commande/component/card.svelte
+++ src/routes/events/[slug]/commande/component/card.svelte
@@ -10,7 +10,12 @@
 		<p>Ticket</p>
 		<div class="form-group"></div>
 		<input type="hidden" id={data.price.id} name="tiers" value={JSON.stringify(data.price)} />
-		<p>{data.price.label} : {(data.price.price / 100).toFixed(2).replace('.', ',')} €</p>
+		<p>
+			{data.price.label} : {(data.price.price / 100).toFixed(2).replace('.', ',')} €{data.info
+				.payForPartner
+				? ' x2'
+				: ''}
+		</p>
 		<button class="btn" type="submit">Réserver mon billet</button>
 	</form>
 </div>
